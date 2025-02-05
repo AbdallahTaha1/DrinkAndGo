34b0a8391af73128e849d0cf691b316e4931af3c
AbdallahTaha1 <engbody007@gmail.com>
2025-02-05 16:39:41 +0200

    AddLoginMethods

---
+++ Controllers/AccountController.cs
@@ -0,0 +1,44 @@
+﻿using DrinkAndGo.ViewModels;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DrinkAndGo.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly SignInManager<IdentityUser> _signInManager;
+        private readonly UserManager<IdentityUser> _userManager;
+
+        public AccountController(SignInManager<IdentityUser> signInManager, UserManager<IdentityUser> userManager)
+        {
+            _signInManager = signInManager;
+            _userManager = userManager;
+        }
+
+        public IActionResult Login(string returnUrl)
+        {
+            return View(new LoginViewModel() { ReturnUrl = returnUrl});
+        }
+        [HttpPost]
+        public async Task<IActionResult> Login(LoginViewModel model)
+        {
+            if (!ModelState.IsValid) 
+                return View(model);
+
+            var user = await _userManager.FindByNameAsync(model.UserName);
+            if (user != null)
+            {
+                var result = await _signInManager.PasswordSignInAsync(user, model.Password, false, false);
+                if (result.Succeeded)
+                {
+                    if (String.IsNullOrEmpty(model.ReturnUrl))
+                        return RedirectToAction("Index", "Home");
+                    return Redirect(model.ReturnUrl);
+                }               
+            }
+            ModelState.AddModelError("", "UserName/Password Not Found!");
+            return View(model);
+
+        }
+    }
+}
--- Migrations/20250205135231_addIdentityTables.Designer.cs
+++ Migrations/20250205135231_addIdentityTables.Designer.cs
@@ -4,6 +4,7 @@ using DrinkAndGo.Data.Models;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace DrinkAndGo.Migrations
 {
     [DbContext(typeof(DrinkAndGoContext))]
-    partial class DrinkAndGoContextModelSnapshot : ModelSnapshot
+    [Migration("20250205135231_addIdentityTables")]
+    partial class addIdentityTables
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
